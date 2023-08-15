aa5684ff2e32431d91eb611fca55e815f3c9ae34
Hasan Oruc <65034033+Oruch379@users.noreply.github.com>
2023-08-15 11:20:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className=''>
       <div className='flex p-40 justify-between items-center shrink-0'>
-        <img src={Zeyno} alt="profile" className='rounded-full h-52 w-52 object-cover animate-bounce' />
+        <img src={Zeyno} alt="profile" className='rounded-full h-52 w-52 object-cover animate-pulse' />
         <p className='break-words max-w-sm text-center'>Hasan Oruc tarafından yapılan bir deneme sayfası. Portfolio olarak tasarlandı.</p>
       </div>
     </div>
