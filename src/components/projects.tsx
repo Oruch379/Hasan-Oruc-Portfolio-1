2841decb9db7fa7cf727a1b6125e048c211e593e
Hasan Oruc <oruch379@gmail.com>
2023-09-16 15:41:12 +0300

    Changes

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,11 +3,11 @@ import Project from "./projectsCard";
 
 export default function Projects() {
   return (
-    <>
-      <h1 className="flex justify-center mt-20 mb-14 text-xl font-semibold hover:scale-110 hover:text-bgred transition">
+    <div id="projects" className="pt-24"> 
+      <h1 className="flex justify-center mb-14 text-xl font-semibold hover:scale-110 hover:text-bgred transition">
         Projects
       </h1>
-      <div id="projects" className="flex md:px-10 p-5 mb-16 justify-evenly flex-wrap gap-4">
+      <div className="flex md:px-10 p-5 mb-16 justify-evenly flex-wrap gap-4 md:gap-y-5">
         <Project
           title="Deneme"
           description="sldhnfnljd snbk jsdbfjlbdsf jklbsdklfbjsldbfsbdfbn"
@@ -24,6 +24,6 @@ export default function Projects() {
           imgUrl="https://upload.wikimedia.org/wikipedia/tr/thumb/f/f5/MustafaKemalUniversitesiHatay.png/200px-MustafaKemalUniversitesiHatay.png"
         />
       </div>
-    </>
+    </div>
   );
 }
